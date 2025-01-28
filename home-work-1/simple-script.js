171095a91b7492609e9b850801787878544b9957
Maksym Hekalo <maximgekalo@gmail.com>
2025-01-28 09:45:23 +0200

    create function for get biggest number

---
+++ home-work-1/simple-script.js
@@ -0,0 +1,17 @@
+
+/**
+ * @param {number} numbers
+ * @returns {number}
+ */
+function getBiggestNumber(numbers) {
+  let biggestNumber = numbers[0];
+  
+  for (const number of numbers) {
+    if (biggestNumber < number) {
+      biggestNumber = number;
+    } 
+  }
+
+  return biggestNumber
+
+}
